89c24be3722fbb19f8707e41463ffd5a4c42122c
renee.paternesi@coderhouse.com <renee.paternesi@coderhouse.com>
2022-05-26 18:35:18 -0300

    proyecto2

---
+++ src/services/services.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export default {
+  async getProducts () {
+    return await axios.get('https://628e6ce6a339dfef87adf203.mockapi.io/reneeshoes/products')
+  },
+  async getProduct (productId) {
+    return await axios.get('https://628e6ce6a339dfef87adf203.mockapi.io/reneeshoes/products', productId)
+  },
+  async addProduct (product) {
+    return await axios.post('https://628e6ce6a339dfef87adf203.mockapi.io/reneeshoes/product', product)
+  },
+  async getUsers () {
+    return await axios.get('https://628e6ce6a339dfef87adf203.mockapi.io/reneeshoes/users')
+  },
+  async getUser (userId) {
+    return await axios.get('https://628e6ce6a339dfef87adf203.mockapi.io/reneeshoes/users', userId)
+  },
+  async addUser (user) {
+    return await axios.post('https://628e6ce6a339dfef87adf203.mockapi.io/reneeshoes/user', user)
+  },
+  async addOrder (order) {
+    return await axios.post('https://628e6ce6a339dfef87adf203.mockapi.io/reneeshoes/order', order)
+  },
+}
